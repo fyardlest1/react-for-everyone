a3775f933d56a87be8ec20664ad1a4681e83079d
fyardlest <fyardlest1@gmail.com>
2020-11-30 21:28:29 -0500

    Detail Page CSS

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -4,6 +4,10 @@ import { useParams } from 'react-router-dom'
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const API_URL = '?api_key=1d8a5b8800903f3add9a06720fcf8962'
 
+const IMAGE_URL = 'http://image.tmdb.org/t/p/'
+const BACKDROP_SIZE = 'original'
+const POSTER_SIZE = 'w342'
+
 export const MovieDetails = () => {
     const { id } = useParams()
     const [movie, setMovie] = useState({})
@@ -27,15 +31,28 @@ export const MovieDetails = () => {
 
     return (
         <div>
-            <h1> {movie.title} </h1>
-            <p> {movie.overview} </p>
-            <ul>
-              {movie.genres.map(genre => (
-                <li key={genre.id}>
-                  {genre.name}
-                </li>
-              ))}
-            </ul>
+          <img 
+            className='backdrop' 
+            src={`${IMAGE_URL}${BACKDROP_SIZE}${movie.backdrop_path}`} 
+            alt={`${movie.title} Backdrop`} />
+          <div className="detail-details">
+            <img 
+              className='detail-poster' 
+              src={`${IMAGE_URL}${POSTER_SIZE}${movie.poster_path}`} 
+              alt={`${movie.title} Poster`} 
+            />
+            <div>
+              <h1> {movie.title} </h1>
+              <p> {movie.overview} </p>
+              <ul>
+                {movie.genres.map(genre => (
+                  <li key={genre.id}>
+                    {genre.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
     )
 }
